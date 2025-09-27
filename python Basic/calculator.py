d6e87ea741d4bb3e4e6de4f65cbf137f962eca89
aryanandini06 <155985716+aryanandini06@users.noreply.github.com>
2025-09-27 10:03:34 +0530

    Add files via upload

---
+++ python Basic/calculator.py
@@ -0,0 +1,23 @@
+# simple calculator
+
+# input number
+num1= float(input("write first no:"))
+num2= float(input("write  second no:"))
+
+# performing arithmetic operation
+sum= num1+num2 
+subtracton = num1- num2 
+multipication = num1*num2
+division = num1/ num2
+floor_division = num1// num2
+module = num1 % num2
+exponentiation = num1 ** num2
+
+# display result
+print("Addition:", sum)
+print("subtraction:", subtracton)
+print("multiplication:", multipication)
+print("division:", division)
+print("floor_division:", floor_division)
+print("module:", module)
+print("exponentiation:", exponentiation)
